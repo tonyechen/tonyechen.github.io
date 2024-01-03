2480cf39cf3837aa01497e04f24b089b64f72bb6
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2024-01-03 16:58:08 -0500

    updated resume - added e96 instructor on my resume

--- components/projects/Project.js
+++ components/projects/Project.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// TODO: expand these to fill the wholon
+// include descriptions
+// link and stuff
 const Project = (props) => {
   return <a className="project__container" href={props.link}>
     <img src={props.image} alt="image" className="project__image"/>
